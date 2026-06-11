766baa5979da8156151de99a5fe128048f20d9a2
Christian Castro <ccastro.ju73@gmail.com>
2026-06-11 12:10:07 -0600

    Nuevo Diseño pagina resumen

--- src/Resultados.jsx
+++ src/Resultados.jsx
@@ -1,5 +1,6 @@
 import MatchResultCard from './MatchResultCard';
 
+
 export default function Resultados({ matches }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
